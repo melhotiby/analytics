679359fe3bbc52e7eb08e9ece4938ff18015087e
Matt <hotiby@gmail.com>
2014-01-30 20:37:01 -0500

    mongo

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+"use strict";
+
+
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Compile SASS files into CSS
+    compass: {
+      dev: {
+        options: {
+          sassDir:'sass',
+          cssDir: 'public/stylesheets',
+          outputStyle: 'compact',
+          require: 'zurb-foundation'
+        }
+      }
+    },
+
+
+    // Watch changes on these files
+    watch: {
+      server: {
+        files: ['sass/**/*.scss'],
+        tasks:['compass:dev']
+      }
+    }
+
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+
+  // Run these tasks on load
+  grunt.task.run('compass:dev')
+};
+
